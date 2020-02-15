f9fded1681992bb3b5ff03e6914d7545a78c36e8
adaao <armascarelli@gmail.com>
2020-02-15 08:22:06 -0300

    === e ==

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+// o "===" ou "==" são funções infixas  
+//que retornam um bool e seu tipo é:
+// === :: a -> a -> Bool
+// == :: a -> a -> Bool
+//exemplo:
+
+var sexo = 'M';
+
+var masculino = (sexo === 'M');
+
+console.log(masculino);
