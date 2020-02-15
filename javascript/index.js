1de829cccd54a45cc1d9b9f03e6d9e8e517209b4
adaao <armascarelli@gmail.com>
2020-02-15 08:51:42 -0300

    set interval/timeout

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,5 @@
-// o "===" ou "==" são funções infixas  
-//que retornam um bool e seu tipo é:
-// === :: a -> a -> Bool
-// == :: a -> a -> Bool
-//exemplo:
-
-var sexo = 'M';
-
-var masculino = (sexo === 'M');
-
-console.log(masculino);
+function exibeAlgo(){
+   console.log('Hello, World!');
+}
+//setTimeout
+setInterval(exibeAlgo, 1000);
