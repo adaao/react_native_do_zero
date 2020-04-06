0c22c47669dd5f6468939193ec659913102086c7
adaao <armascarelli@gmail.com>
2020-04-06 09:22:12 -0300

    ES6 - Módulo 1 - arrow functions, valores padrao, desestruturacao, operadores rest spread

--- es6/main.js
+++ es6/main.js
@@ -35,3 +35,56 @@ const find = arr.find(function(item){
 })
 
 console.log(find);
+
+function soma(a = 2, b = 3){
+   return a + b;
+}
+
+const soma_ = (a = 2, b = 3) => a + b;
+
+//====================================================================
+
+const usuario = {
+   nome: "Adaão", 
+   idade: 34,
+   empresa: "Rocketseat", 
+   endereco: {
+      cidade: "Santos",
+      estado: "SP",
+
+   },
+};
+
+//const { nome, idade, endereco: { cidade } } = usuario;
+
+const mostraInfo = ({ nome, idade }) => console.log(nome, idade);
+
+//==========================================================================
+
+//REST
+
+const { nome, ...resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+function fatorial(...params){
+   return params.reduce((total, next) => total * next);
+}
+
+console.log(fatorial(1, 2, 3, 4, 5));
+
+function leftovers(a, b, ...params){
+   console.log(params);
+}
+
+leftovers(1, 2, 3, 4, 5, 6);
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+
+var arr3 = [...arr1, ...arr2];
+
+const usuario2 = {...usuario, nome: "Raquel" }
+
+console.log(usuario2);
