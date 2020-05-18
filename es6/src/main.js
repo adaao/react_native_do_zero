3f1a968c50ddd28a24730f93437d49d2e29566cb
adaao <armascarelli@gmail.com>
2020-05-18 08:48:32 -0300

    ES6 - Módulo 4 - estrutura e estilos

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,17 +1,1 @@
-import axios from 'axios';
-
-class Api {
-   static async getUserInfo(userName){
-      try{
-         const response = await axios.get(`http://api.github.com/users/${userName}`);
-
-         console.log(response);
-      } catch (err) {
-         console.log('Usuário não encontrado ou erro na Api');
-      }
-   }
-}
-
-Api.getUserInfo('adaao');
-
-Api.getUserInfo('diego3g41234123423');
+console.log('foo');
