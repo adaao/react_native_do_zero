6271f5e86818e1f5673c224808014804a6b4aad2
adaao <armascarelli@gmail.com>
2020-05-19 08:37:33 -0300

    ES6 - Módulo 4 - Adicionar repositorios

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,1 +1,27 @@
-console.log('foo');
+class App{
+   constructor(){
+      this.repositories = [];
+
+      this.formEl = document.querySelector('#repo-form');
+      this.registerHandlers();
+   }
+
+   registerHandlers(){
+      this.formEl.onsubmit = event => this.addRepository(event);
+   }
+
+   addRepository(event){
+      event.preventDefault();
+
+      this.repositories.push({
+         name: 'a',
+         description: 'b',
+         avatar_url: 'c',
+         html_url: 'd'
+      });
+
+      console.log(this.repositories);
+   }
+}
+
+new App();
