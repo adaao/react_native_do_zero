67ec7adb639e9f3eb8301953d32c123d5fe6e036
adaao <armascarelli@gmail.com>
2020-05-22 07:02:02 -0300

    ES6 - Módulo 4 - Buscando da api

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App{
    constructor(){
       this.repositories = [];
 
       this.formEl = document.querySelector('#repo-form');
+      this.inputEl = document.querySelector('input[name=repository]');
       this.listEl = document.querySelector('#repo-list');
 
       this.registerHandlers();
@@ -13,15 +16,26 @@ class App{
       
    }
 
-   addRepository(event){
+   async addRepository(event){
       event.preventDefault();
 
+      const repoInput = this.inputEl.value;
+
+      if (repoInput.length === 0)
+         return;
+
+      const response = await api.get(`/repos/${repoInput}`);
+
+      const { name, description, html_url, owner:{ avatar_url } } = response.data;
+
       this.repositories.push({
-         name: 'rocketseat.com.br',
-         description: 'Tire a sua ideia do papel e de vida à sua startup!',
-         avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
-         html_url: 'http://github.com/Rocketseat/'
+         name,
+         description,
+         avatar_url,
+         html_url,
       });
+
+      this.inputEl.value = '';
 
       this.render();
    }
