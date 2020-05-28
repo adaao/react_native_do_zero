c8c6bdc14f9908de0ad84af21562a4dbf860f2b3
adaao <armascarelli@gmail.com>
2020-05-28 08:16:30 -0300

    NodeJs - Aula 7 - Conectando DB

--- NodeJs/node-api/server.js
+++ NodeJs/node-api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//iniciando o app
 const app = express();
 
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+//primeira rota
 app.get('/', (req, res) => {
    res.send('Hello to adaao');
 });
 
 app.listen(3001);
+
